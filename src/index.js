ddba2a5e4e57abbfd230aaaaeb5da3c94121a601
Susan Dreher <sdreher@columbia.edu>
2016-07-26 15:38:32 -0400

    migration

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/* global jQuery: true */
+
+require('!file?name=[name].[ext]!../static/index.html');
+require('./static.js');
+
+// load and apply css
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('../node_modules/bootstrap-arrow-buttons/dist/' +
+        'css/bootstrap-arrow-buttons.css');
+require('../static/css/common.css');
+require('../static/css/infographic.css');
+
+var jQuery = require('jquery');
+var module = require('./infographic.js');
+
+jQuery(document).ready(function() {
+    module.InfographicApp.initialize();
+});
---
+++ src/infographic.js
@@ -0,0 +1,92 @@
+/* global jQuery: true, module: true */
+
+jQuery = require('jquery');
+var Backbone = require('backbone');
+var _ = require('underscore');
+var models = require('./models.js');
+window.jQuery = window.$ = jQuery;
+require('bootstrap');
+
+var InfographicView = Backbone.View.extend({
+    events: {
+        'hover #imageMapArea area': 'onHover',
+        'click #imageMapArea area': 'onClick',
+    },
+    initialize: function(options) {
+        _.bindAll(this, 'render', 'progress', 'onHover', 'onClick');
+
+        var self = this;
+        this.template = require('../static/templates/page.html');
+
+        var data = require('../static/json/items.json');
+        this.items = new models.ItemList(data);
+
+        for (var i = 0; i < this.items.length; i++) {
+            this.items.at(i).bind('change', self.render);
+        }
+
+        jQuery('.btn-print').click(this.onPrint);
+
+        this.render();
+    },
+    progress: function() {
+        var pct = this.items.clicked() / this.items.length;
+        return Math.round(100 * pct);
+    },
+    render: function() {
+        var context = {
+            'items': this.items.toTemplate(),
+            'clicked': this.items.clicked(),
+            'shim': './shim.gif',
+            'percentDone': this.progress()
+        };
+
+        var markup = this.template(context);
+        jQuery(this.el).html(markup);
+
+        this.maybeComplete();
+    },
+    onHover: function(evt) {
+        var itemContent = jQuery(evt.target).attr('href');
+        var itemPosition = 'item-position-' + itemContent;
+        var itemImage = 'item-image-' + itemContent;
+        jQuery('#' + itemPosition).addClass(itemImage + '-hover');
+    },
+    onClick: function(evt) {
+        evt.preventDefault();
+        var $elt = jQuery(evt.target);
+        var item = this.items.getByDataId($elt.data('id'));
+        item.set('clicked', true);
+
+        var $modal = this.$el.find('#item-modal');
+        $modal.find('.modal-title').html(item.get('label_name'));
+        $modal.find('.item-thumbnail')
+            .attr('id', 'thumbnail-' + item.get('label'));
+        $modal.find('.item-content').html(item.get('content'));
+        $modal.modal({});
+
+        return false;
+    },
+    onPrint: function(evt) {
+        evt.preventDefault();
+        window.print();
+        return false;
+    },
+    maybeComplete: function() {
+        if (this.items.clicked() === this.items.length) {
+            jQuery('.activity-complete').show();
+        }
+    }
+});
+
+var InfographicApp = {
+    initialize: function(options) {
+        var infographicView = new InfographicView({
+            el: jQuery('.infographic')
+        });
+
+        jQuery('body').show();
+    }
+};
+
+module.exports.InfographicApp = InfographicApp;
