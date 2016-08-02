e01c11975ab34fa17bec41bba1412c21d576144a
Susan Dreher <sdreher@columbia.edu>
2016-08-02 10:39:16 -0400

    npm3: remove relative references to node_modules

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ require('!file?name=[name].[ext]!../static/index.html');
 require('./static.js');
 
 // load and apply css
-require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
-require('../node_modules/bootstrap-arrow-buttons/dist/' +
-        'css/bootstrap-arrow-buttons.css');
+require('!style!css!bootstrap/dist/css/bootstrap.min.css');
 require('../static/css/common.css');
 require('../static/css/infographic.css');
 
