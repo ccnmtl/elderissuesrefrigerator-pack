482d8efed8cab01e6d3221733674e5d185dfce73
Susan Dreher <sdreher@columbia.edu>
2017-02-27 10:13:10 -0500

    Use long-form -loader syntax

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 /* global jQuery: true */
 
-require('!file?name=[name].[ext]!../static/index.html');
+require('!file-loader?name=[name].[ext]!../static/index.html');
 require('./static.js');
 
 // load and apply css
-require('!style!css!bootstrap/dist/css/bootstrap.min.css');
-require('../static/css/common.css');
-require('../static/css/infographic.css');
+require('!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
+require('!style-loader!css-loader!../static/css/common.css');
+require('!style-loader!css-loader!../static/css/infographic.css');
 
 var jQuery = require('jquery');
 var module = require('./infographic.js');
 
 jQuery(document).ready(function() {
-    module.InfographicApp.initialize();
+    module.InfographicApp.initialize({'background': 'fridge_print.jpg'});
 });
