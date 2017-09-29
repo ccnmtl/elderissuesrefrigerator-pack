fbe7e989a20db3fe8de863655622496f1abdd314
Susan Dreher <sdreher@columbia.edu>
2017-09-29 12:37:34 -0400

    eslint: enhanced security checks

--- test/view-test.js
+++ test/view-test.js
@@ -1,4 +1,4 @@
-/* global describe: true, before: true, it: true */
+/* global describe: true, before: true */
 
 require('!file-loader?name=[name].[ext]!./view-test.html');
 require('../src/static.js');
@@ -9,31 +9,13 @@ var assert = chai.assert;
 var jQuery = require('jquery');
 var module = require('../src/infographic.js');
 
-function waitFor(testFx, doneFx, millis) {
-    var timeout = millis ? millis : 3000; // Default Max Timout is 10s
-    var start = new Date().getTime();
-
-    var interval = setInterval(function() {
-        var condition = testFx();
-
-        if (condition) {
-            clearInterval(interval);
-            doneFx();
-        } else if ((new Date().getTime() - start >= timeout)) {
-            clearInterval(interval);
-            doneFx(new Error('timeout occurred'));
-        }
-    }, 250); //< repeat check every 250ms
-}
-
 describe('InfographicApp', function() {
-    var app;
 
     before(function() {
         var elt = jQuery('.infographic');
         assert.isDefined(elt);
         jQuery(elt).html('');
 
-        app = module.InfographicApp.initialize();
+        module.InfographicApp.initialize();
     });
 });
